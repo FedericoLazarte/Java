b5e2b84980b7387aecbe12fe3f6763aa0fa5ea49
FedericoLazarte <fedelazarte2015@gmail.com>
2026-01-01 17:35:57 -0300

    add insertAsteriskToString

--- ProgramacionI/src/practica0/Ejercicio34.java
+++ ProgramacionI/src/practica0/Ejercicio34.java
@@ -1,5 +1,21 @@
 package practica0;
 
 public class Ejercicio34 {
-
+	
+	public static void insertAsteriskToString(String s) {
+		if (s == null) throw new IllegalArgumentException("La palabra ingresada no puede ser nula.");
+		if (s.isEmpty()) return;
+		if (s.length() == 1) System.out.print(s.charAt(0));
+		else System.out.print(s.charAt(0) + "*");
+		
+		insertAsteriskToString(rest(s));
+	}
+	
+	private static String rest(String s) {
+		return s.substring(1);
+	}
+	
+	public static void main(String[] args) {
+		insertAsteriskToString("Hola");
+	}
 }
